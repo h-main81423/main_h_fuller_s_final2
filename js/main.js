d158db34e340490aa2ee25da6057e3277aa7fc3b
Hayden Main <hayden.main2@gmail.com>
2019-04-13 18:03:29 -0400

    added js scroll, finalized all responsive design

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@
 	var button = document.querySelector("#burger");
 	var burgerCon = document.querySelector("#burgerCon");
 	//variables for returning to top of page
-	let returns = document.querySelector("#mobileFoot button");
-	let targetArea;
+	var goTopBtn = document.querySelector('#return');
 	//menu dropdown code
 	var accordion =  document.querySelectorAll(".toggle");
+
 
 
 	//FUNCTIONS
@@ -23,14 +23,27 @@
 		button.classList.toggle("expanded");
 	}
 
-	//return to top function .... not sure how to get it to work though..
-	function scroll(e) {
-		console.log("scroll called");
-		e.currentTarget.id.slice(0,-3);
-		TweenLite.to(window, 1, {scrollTo:{y:`#${targetArea}`, offsetY:70, autoKill:false}} );
-	}
+	//function for setting up scroll, selecting the window and the Y offset
+	function returnToTop() {
+		//scroll variable, pins where on the page user is.
+	    var scrolling = window.pageYOffset;
+	    //coordinate variable, gets height of page
+	    var coords = document.documentElement.clientHeight;
 
-	console.log(returns);
+	  }
+	 //scrolling to top
+	function toStart() {
+		//logs when the function is called
+		console.log('scroll fired');
+
+		//if user is more than 0px down the window will scroll back to the top.
+	    if (window.pageYOffset > 0) {
+	      window.scrollBy(0, -80);
+	      setTimeout(toStart, 0);
+	    }
+
+	  }
+
 
 	//drop down menu for newsletters
 	function dropdown(e) {
@@ -41,21 +54,20 @@
 
 		e.currentTarget.nextElementSibling.classList.toggle('opened');
 	}
+
 	
 	//EVENT HANDLING
 
 	//menu event
 	button.addEventListener("click", menu, false);
 
-	//for loop event listener
-	for(var i=0; i < returns.length; i++){
-		//link[0] access first link in node list
-		returns[i].addEventListener("click", scroll);
-	}
 
 	//dropdown event
 	for(var i=0; i < accordion.length; i++) {
 		accordion[i].addEventListener("click", dropdown);
 	}
+
+	  window.addEventListener('scroll', returnToTop);
+	  goTopBtn.addEventListener('click', toStart);
 
 })();
